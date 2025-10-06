ddb62ebfe7d965c96177de0f5874c5338025e052
gmutafov <georgimutafovbg@gmail.com>
2025-10-06 16:48:04 +0300

    complete database setup

--- backend/cloud_env_manager/settings.py
+++ backend/cloud_env_manager/settings.py
@@ -67,11 +67,10 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
-        'PORT': os.getenv('POSTGRES_PORT'),
+        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', 5432),
     }
 }
-
 
 
 # Password validation
